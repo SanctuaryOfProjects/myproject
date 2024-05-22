3cafc297c60db3db93be2f9c8946f6372ed2c184
SanctuaryOfProjects <dzhumabayevakarina@gmail.com>
2024-05-23 01:42:05 +0500

    - meh

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-1c15.up.railway.app', 'localhost', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ["web-production-1c15.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-1c15.up.railway.app"]
 
 # Application definition
 
